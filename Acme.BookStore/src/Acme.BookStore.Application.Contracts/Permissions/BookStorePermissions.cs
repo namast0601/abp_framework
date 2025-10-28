782463d445e23260609ec3629f3ece921fecbae9
namast0601 <108456121+namast0601@users.noreply.github.com>
2025-10-28 16:18:43 +0700

    add authorization

--- Acme.BookStore/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ Acme.BookStore/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -8,4 +8,12 @@ public static class BookStorePermissions
     
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
+    // *** ADDED a NEW NESTED CLASS ***
+    public static class Books
+    {
+        public const string Default = GroupName + ".Books";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
+    }
 }
